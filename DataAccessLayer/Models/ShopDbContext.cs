0fc080ef3daa6a7e74a6ab4d9f04d5104e42956b
Dat LT <datlt.mdc@gmail.com>
2025-09-30 13:54:31 +0700

    Fix database

--- DataAccessLayer/Models/ShopDbContext.cs
+++ DataAccessLayer/Models/ShopDbContext.cs
@@ -25,15 +25,15 @@ public partial class ShopDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(local);user=sa;password=12345;Database=ShopDb;Trusted_Connection=True;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Server=(local);user=sa;password=12345;Database=ShopDB;Trusted_Connection=True;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Customer>(entity =>
         {
-            entity.HasKey(e => e.CustomerId).HasName("PK__Customer__A4AE64D859E308F0");
+            entity.HasKey(e => e.CustomerId).HasName("PK__Customer__A4AE64D829D181A4");
 
-            entity.HasIndex(e => e.Email, "UQ__Customer__A9D1053464FB162F").IsUnique();
+            entity.HasIndex(e => e.Email, "UQ__Customer__A9D105342CAD6368").IsUnique();
 
             entity.Property(e => e.Email).HasMaxLength(255);
             entity.Property(e => e.FullName).HasMaxLength(255);
@@ -42,34 +42,34 @@ public partial class ShopDbContext : DbContext
 
         modelBuilder.Entity<Order>(entity =>
         {
-            entity.HasKey(e => e.OrderId).HasName("PK__Orders__C3905BCF76242A47");
+            entity.HasKey(e => e.OrderId).HasName("PK__Orders__C3905BCF316EC350");
 
             entity.HasOne(d => d.Customer).WithMany(p => p.Orders)
                 .HasForeignKey(d => d.CustomerId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__Orders__Customer__29572725");
+                .HasConstraintName("FK__Orders__Customer__3C69FB99");
         });
 
         modelBuilder.Entity<OrderDetail>(entity =>
         {
-            entity.HasKey(e => e.OrderDetailId).HasName("PK__OrderDet__D3B9D36CFF53F226");
+            entity.HasKey(e => e.OrderDetailId).HasName("PK__OrderDet__D3B9D36C3993FC66");
 
             entity.Property(e => e.UnitPrice).HasColumnType("money");
 
             entity.HasOne(d => d.Order).WithMany(p => p.OrderDetails)
                 .HasForeignKey(d => d.OrderId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__OrderDeta__Order__2C3393D0");
+                .HasConstraintName("FK__OrderDeta__Order__3F466844");
 
             entity.HasOne(d => d.Product).WithMany(p => p.OrderDetails)
                 .HasForeignKey(d => d.ProductId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK__OrderDeta__Produ__2D27B809");
+                .HasConstraintName("FK__OrderDeta__Produ__403A8C7D");
         });
 
         modelBuilder.Entity<Product>(entity =>
         {
-            entity.HasKey(e => e.ProductId).HasName("PK__Products__B40CC6CDCC2F5136");
+            entity.HasKey(e => e.ProductId).HasName("PK__Products__B40CC6CD31ADD5C6");
 
             entity.Property(e => e.Name).HasMaxLength(255);
             entity.Property(e => e.Price).HasColumnType("money");
